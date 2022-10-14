226a5fc4852e845e871e6419235977cae39dd301
EL.MLIKI.HICHAM <92031524+El-Mliki-Hicham@users.noreply.github.com>
2022-10-14 22:52:40 +0200

    create tables db

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Providers;
+use Illuminate\Support\Facades\Schema;
 
 use Illuminate\Support\ServiceProvider;
 
@@ -21,8 +22,10 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
-    {
-        //
-    }
+
+// dans la méthode boot(), inclure la ligne suivante
+public function boot()
+{
+    Schema::defaultStringLength(191);
+}
 }
--- database/migrations/2022_10_14_194618_create_students_table.php
+++ database/migrations/2022_10_14_194618_create_students_table.php
@@ -13,10 +13,45 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('students', function (Blueprint $table) {
-            $table->id();
+        Schema::create('promotions', function (Blueprint $table) {
+            $table->increments("Id_promotion");
+            $table->string('Name_Promotion')->nullable();
             $table->timestamps();
+            });
+        Schema::create('students', function (Blueprint $table) {
+        $table->increments("Id_student");
+        $table->string('Name_student')->nullable();
+        $table->string('Age')->nullable();
+        $table->unsignedInteger('Promotion')->nullable();
+        $table->foreign('Promotion')
+        ->references('Id_promotion')
+        ->on('promotions')
+        ->onDelete('cascade');
+        $table->timestamps();
         });
+    
+    
+        // Schema::create('Promotions', function (Blueprint $table) {
+        //     $table->increments("id_promotion");
+        //     $table->string('Name_promotion')->nullable();
+        //     $table->string('')->nullable();
+        //     $table->timestamps();
+
+        // });
+        // Schema::create('exercices', function (Blueprint $table) {
+        //     $table->increments("id_exercice")->nullable();
+        //     $table->string('nom_exercice')->nullable();
+        //     $table->unsignedInteger('categorie_exercice')->nullable();
+        //     $table->string('description_exercice')->nullable();
+        //     $table->string('repetition_exercice')->nullable();
+        //     $table->string('photo_exercice')->nullable();
+        //     $table->foreign('categorie_exercice')
+        //     ->references('id_categorie_exercice')
+        //     ->on('categories_exerices')
+        //     ->onDelete('cascade');
+        //     $table->timestamps();
+
+        // });
     }
 
     /**

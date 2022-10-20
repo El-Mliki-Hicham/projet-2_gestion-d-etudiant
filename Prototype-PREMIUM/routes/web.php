fd762b6da11b9aef50dfdfea397faee1c77fe13a
EL.MLIKI.HICHAM <92031524+El-Mliki-Hicham@users.noreply.github.com>
2022-10-20 07:45:44 -0700

    delete

--- Prototype-PREMIUM/routes/web.php
+++ Prototype-PREMIUM/routes/web.php
@@ -19,6 +19,7 @@ Route::get('index',[PromotionController::class,'index']);
 Route::get('create',[PromotionController::class,'create']);
 Route::post('Add',[PromotionController::class,'store']);
 Route::get('Edit/{id}',[PromotionController::class,'Edit']);
+Route::get('Delete/{id}',[PromotionController::class,'Delete']);
 Route::post('Update/{id}',[PromotionController::class,'Update']);
 Route::get('search',[PromotionController::class,'search']);
 

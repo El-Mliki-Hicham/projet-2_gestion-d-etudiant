302660c9b6d60f5ac2860e2bf94c0d45c3cceb7b
EL.MLIKI.HICHAM <92031524+El-Mliki-Hicham@users.noreply.github.com>
2022-10-21 05:44:40 -0700

    add template and style

--- Realisation/resources/views/promotion/index.blade.php
+++ Realisation/resources/views/promotion/index.blade.php
@@ -1,24 +1,75 @@
-{{-- {{$promotion}} --}}
-<a href="create"><button>ajouter</button></a>
-<table>
+@include('layouts.head')
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
 
-    <thead>
-        <tr>
-            <th>Id</th>
-            <th>Nom promotion</th>
-            <th>Action</th>
-        </tr>
-    </thead>
-    @foreach ($promotion as $item)
-    <tbody>
-        <tr>
-            <td>{{$item->Id_promotion}}</td>
-            <td>{{$item->Name_promotion}}</td>
-            <td>
-                <a href="Edit/{{$item->Id_promotion}}">Edit</a>
-                <a href="Delete/{{$item->Id_promotion}}">Delete</a>
-            </td>
-        </tr>
-    </tbody>
-    @endforeach
-</table>
+@if (session('status'))
+<h6 class="alert alert-success">{{ session('status') }}</h6>
+@endif
+
+
+<script type="text/javascript" src="{{ URL::asset('js/search.js') }}"></script>
+
+
+<!doctype html>
+<html lang="en">
+ 
+<body>
+   
+<script>
+$(document).ready(function(){
+	$('[data-toggle="tooltip"]').tooltip();
+});
+</script>
+
+
+
+<div class="container">
+    <div class="titleDiv">
+    <p><i class="fa-solid fa-user-graduate iconStudent"> </i></p>
+    <h1 class="titleGestion"> Gestion d'etudiant</h1>
+    
+    </div>
+    <div class="card cadre-search">
+        <div class="card-body">
+            <div class="col-sm-4">
+                <div class="search-box">
+                    <i class="material-icons">&#xE8B6;</i>
+                    <input type="text" class="form-control searchInput" placeholder="Search&hellip;">
+                </div>
+            </div>
+            <div class="col-sm-8 divAdd" >
+                <button type="button" class="btn btn-info add-new"><i class="fa fa-plus"></i> Add New</button>
+            </div>
+        </div>
+      </div>
+    <div class="table-responsive">
+        <div class="table-wrapper">
+           
+            <table class="table table-striped table-hover">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Name promotion </th>
+                        <th scope="col">Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @forelse  ($promotion as $item)
+                    <tr>
+                        <td scope="row">{{$item->Id_promotion}}</td>
+                        <td>{{$item->Name_promotion}}</td>
+                        <td>
+    
+                    <a href="Edit/{{$item->Id_promotion}}" class="settings" title="Edit" data-toggle="tooltip"><i class="fa-regular fa-pen-to-square"></i></a>
+                    <a href="Delete/{{$item->Id_promotion}}" class="delete" title="Delete" data-toggle="tooltip"><i class="fa-solid fa-trash"></i></a>
+                        </td>
+                @empty
+        
+                @endforelse
+                   </tr>                                     
+                </tbody>
+            </table>
+           
+        </div>
+    </div>        
+</div>     
+</body>
